74273e7c35db47f7b6d6b81120339a65b54dfa46
kenneth palermo <kennethross@aspevo.com>
2020-08-20 16:52:29 +0800

    Setup django admin

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^fxnw#66m_l)#uo3(vgd@)040t+&o4*)q6ld$xwdohf@8a%tfo'
+SECRET_KEY = '&9pm4%pgvzmz*jj465!zg6xql4__d-_wc7pli5bkinig93s0=1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,11 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'core',
-    'user',
-    'product'
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -80,11 +76,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -1,18 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from core import models
-
-def sample_user(email='test@londonappdev.com', password='testpass'):
-    """Create a sample user"""
-    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """ Test Creating new User """
-        email = "admin@gmail.com"
-        password = "12345"
+        """Test creating a new user with an email is successful"""
+        email = 'test@londonappdev.com'
+        password = 'Password123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
@@ -22,28 +17,23 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        email = 'asdas@ASDASDASDAS.com'
-        user = get_user_model().objects.create_user(email, '1222345')
+        """Test the email for a new user is normalized"""
+        email = 'test@LONDONAPPDEV.com'
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
-
+    
     def test_new_superuser(self):
+        """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
             'test@londonappdev.com',
             'test123'
         )
+
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-
-    def test_tag_str(self):
-        """Test the tag string representation"""
-        tag = models.Tag.objects.create(
-            user=sample_user(),
-            name='Cereals'
-        )
-
-        self.assertEqual(str(tag), tag.name)
